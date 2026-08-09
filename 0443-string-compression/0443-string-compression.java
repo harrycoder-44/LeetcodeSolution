dba38c8c55fb5d9d38241dc5b23e3242d2a54113
Harish pawar <harishpawar536@gmail.com>
2026-08-09 18:15:48 +0530

    Time: 3 ms (10.67%), Space: 45.2 MB (90.52%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+
+        sb.append(chars[0]);
+
+        for(int i = 1; i < chars.length; i++){
+            if(chars[i-1] == chars[i]){
+                count++;   
+            } else {
+                if(count > 1){
+                    sb.append(count + "");
+                }
+                sb.append(chars[i]); 
+                count = 1;
+            }
+        }
+
+        if(count > 1){
+            sb.append(count + "");
+        }
+
+        for(int i=0; i<sb.length(); i++){
+            chars[i] = sb.charAt(i);
+        }
+
+        return sb.length();
+    }
+}
